d3c158953b951e0b937a5dee276910839bc8425f
Erich <eheiseler@gmail.com>
2022-02-24 18:09:18 -0500

    commit tree b2d3793a22ed3a7d6d4edde27044646fe8cfb1f0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string) {
+    const matching = array.filter((
+        el => el.toLowerCase() === string.toLowerCase()
+    ))
+    return matching
+}
+
+function fuzzyMatch(array, string) {
+    const fuzzy = array.filter((
+        el => el.slice(0, 2) === string
+    ))
+    return fuzzy
+}
+
+function matchName(objArr, string) {
+    const matching = objArr.filter((
+        el => el.name === string
+    ))
+    return matching
+}
